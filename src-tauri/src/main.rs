635877b19d48a693fe1b6761449b9ac4098f90ad
David Meador <dave.meador@technipfmc.com>
2022-04-17 22:55:35 -0700

    Update to use mutable state for shown/hidden main window - and pass mutable clone through a closure with move

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,13 @@
 use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent};
 use tauri::Manager;
 
+use std::sync::Mutex;
+//use tauri::async_runtime::Mutex;
+use std::sync::Arc;
+
 fn main() {
   // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
-  static mut APP_HIDDEN : bool = false;
+  let app_hidden = Arc::new(Mutex::<bool>::new(false));
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
   let menuitem_show_hide = CustomMenuItem::new("show_hide".to_string(), "Hide");
   let tray_menu = SystemTrayMenu::new()
@@ -19,16 +23,15 @@ fn main() {
 
   tauri::Builder::default()
     .system_tray(SystemTray::new().with_menu(tray_menu))
-    .on_system_tray_event(|app, event| match event {
+    .on_system_tray_event( move |app, event| match event {
       SystemTrayEvent::LeftClick {
         position: _,
         size: _,
         ..
       } => {
         println!("system tray received a left click");
-        let _appwindow = app.get_window("main").unwrap();
-        //window.show().unwrap();
-     
+        //let _appwindow = app.get_window("main").unwrap();
+
       }
       SystemTrayEvent::RightClick {
         position: _,
@@ -52,27 +55,23 @@ fn main() {
             }
             "show_hide" => {
               let window = app.get_window("main").unwrap();
-              let mut local_hidden = false;
-              unsafe {
-                local_hidden = APP_HIDDEN;
-              }
+              let arc  = Arc::clone(&app_hidden);
+              let mut guard = arc.lock().unwrap();
+              let local_hidden = &mut *guard;
 
-              //window.hide().unwrap();
-              if local_hidden == false
+              if *local_hidden == false
               {
-                let _h = item_handle.set_title("Show").unwrap();
+                *local_hidden = true;
                 window.hide().unwrap();
-                local_hidden = true;
+                let _h = item_handle.set_title("Show").unwrap();
               }
               else
               {
-                let _h = item_handle.set_title("Hide").unwrap();
+                *local_hidden = false;
                 window.show().unwrap();
-                local_hidden = false;
+                let _h = item_handle.set_title("Hide").unwrap();
               }
-              unsafe {
-                APP_HIDDEN = local_hidden;
-              }
+
             }
 
             _ => {}
